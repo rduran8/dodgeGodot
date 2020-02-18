d698b6b0462abdaf92d783ffa159864ceb4fd5aa
Roger Duran <rduran8@boscdelacoma.cat>
2020-02-18 17:01:18 +0100

    primer commit

---
+++ scripts/Main.gd
@@ -0,0 +1,56 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	var mob = Mob.instance()
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	mob.position = $MobPath/MobSpawnLocation.position
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	$HUD.connect("start_game", mob, "_on_start_game")
+	
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
